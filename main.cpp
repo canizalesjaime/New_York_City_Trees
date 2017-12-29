29465f9efcbe2d4bbe7a9ed989e3ada447699bcc
Jaime Canizales <canizales.jaime@outlook.com>
2017-12-29 14:22:31 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   main.cpp
- * Author: Caniz
- *
+ * Author: Jaime Canizales
+ 
  * Created on December 29, 2017, 1:08 PM
  */
 
