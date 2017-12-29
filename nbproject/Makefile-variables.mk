fbd2d77bbacc8375c91370737d277d8f1b281642
Jaime C <canizales.jaime@outlook.com>
2017-12-29 13:56:52 -0500

    First Commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=new_york_city_trees
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/new_york_city_trees
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=newyorkcitytrees.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/newyorkcitytrees.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=new_york_city_trees
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/new_york_city_trees
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=newyorkcitytrees.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/newyorkcitytrees.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
